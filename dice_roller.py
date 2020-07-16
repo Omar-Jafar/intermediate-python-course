8925076c8a8cc3a3941216b9d65c000447ee3e10
Omar-Jafar <68290140+Omar-Jafar@users.noreply.github.com>
2020-07-16 15:52:02 +0200

    Update dice_roller.py
    indented all lines under main()

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,6 @@
 def main():
     import random
-  roll = random.randint(1,6)
-  print(f'You rolled a {roll}')
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
 if __name__== "__main__":
   main()
