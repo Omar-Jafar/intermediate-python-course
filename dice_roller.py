c7b1c5408d5318a8ded487df60382d9c12d7fc15
Omar-Jafar <68290140+Omar-Jafar@users.noreply.github.com>
2020-07-16 17:21:45 +0200

    Update dice_roller.py
    indented all lines under main()
    for loop and 2 rolls
    ++ formated

--- dice_roller.py
+++ dice_roller.py
@@ -5,7 +5,8 @@ def main():
     for i in range(0, dice_rolls):
         roll = random.randint(1, 6)
         dice_sum += roll
-        print(f'You rolled a total of {roll}')
+        print(f'You rolled a {roll}')
+    print(f'You have rolled a total of {dice_sum}')
 
 
 if __name__ == "__main__":
